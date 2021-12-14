d632d40ebe89e8aa2ef7d1433cc9f08071a5bfa1
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2021-12-14 16:01:39 +1000

    Clean up duplicate and unnecessary code

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
-    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, BarChart, Bar, Legend
+} from 'recharts';
 
-import {Typography, Button} from "@mui/material";
+import { Typography, Button } from "@mui/material";
 
 
 const ChartTypes = Object.freeze({
@@ -12,227 +13,123 @@ const ChartTypes = Object.freeze({
     Line: "line",
 });
 
-
-import {Typography, Button} from "@mui/material";
-
-function Graph(props){
+function Graph(props) {
 
     const [metricsData, setMetricsData] = useState(null);
-
     const [displayedChartType, setDisplayedChartType] = useState(ChartTypes.Unknown);
 
+    const dataArray = [];
+    const updatedDataArray = [];
+    const [moreData, setMoreData] = useState(metricsData);
+    const [otherData, setOtherData] = useState(metricsData);
+    const [minLatency, setMinLatency] = useState();
+    const [graphTitle, setGraphTitle] = useState();
+
     useEffect(() => {
-        axios.get('/metrics/getdata').then(res => {
-            // console.log(res)
+        axios.get('/metrics/querydb').then(res => {
             const metricsData = res.data;
             setMetricsData(metricsData)
-           
-
         });
     }, []);
 
-  const dataArray = [];
-  const updatedDataArray = [];
-  const [moreData, setMoreData] = useState(metricsData);
-  const [otherData, setOtherData] = useState(metricsData);
-
     useEffect(() => {
-        if (metricsData && metricsData.length > 0){
-            for (let i = 0 ; i < 40; i++) {
-                
+        if (metricsData && metricsData.length > 0) {
+            for (let i = 0; i < 40; i++) {
                 dataArray[i] = {
-                    name: new Date(metricsData[i].ts ).toLocaleString("en-GB", 
-                    {hour: "numeric", minute: "numeric", second: "numeric"}), 
+                    name: new Date(metricsData[i].ts_point).toLocaleString("en-GB",
+                        { hour: "numeric", minute: "numeric", second: "numeric" }),
                     Latency: metricsData[i].avg_lat,
-                    MaxLatency: metricsData[i].avg_max, 
+                    MaxLatency: metricsData[i].avg_max,
                     MinLatency: metricsData[i].avg_min,
-                    ServiceType: metricsData[i].service_type}
-                
+                    ServiceType: metricsData[i].service_type
                 }
-                setMoreData(dataArray)
-
+            }
+            setMoreData(dataArray)
         }
-    },[metricsData])
-
-
+    }, [metricsData])
 
     useEffect(() => {
-        if (metricsData && metricsData.length > 0){
-            for (let i = 0; i < 10; i ++){
+        if (metricsData && metricsData.length > 0) {
+            for (let i = 0; i < 10; i++) {
 
                 updatedDataArray[i] = {
-                    name: new Date(metricsData[i].ts ).toLocaleString("en-GB", 
-                    {hour: "numeric", minute: "numeric", second: "numeric"}), 
+                    name: new Date(metricsData[i].ts_point).toLocaleString("en-GB",
+                        { hour: "numeric", minute: "numeric", second: "numeric" }),
                     Latency: metricsData[i].avg_lat,
-                     MaxLatency: metricsData[i].avg_max, 
-                     MinLatency: metricsData[i].avg_min,
-                    ServiceType: metricsData[i].service_type}
+                    MaxLatency: metricsData[i].avg_max,
+                    MinLatency: metricsData[i].avg_min,
+                    ServiceType: metricsData[i].service_type
+                }
             } setOtherData(updatedDataArray)
             console.log(otherData)
-            
-
-
         }
-    },[metricsData])
-                    
+    }, [metricsData])
 
+    return (
+        <div>
+            <Button variant="contained"
+                onClick={() => {
+                    setMoreData(
+                        otherData);
+                    { setGraphTitle(metricsData[100].service_type + " - Latency Information") }
+                }} > Change Data
+            </Button>
 
+            <Button variant="contained"
+                onClick={() => {
+                    setMinLatency(
+                        "MinLatency");
+                }} > Show Min Latency
+            </Button>
 
+            <Button id="bar" variant="contained"
+                onClick={() => { setDisplayedChartType(ChartTypes.Bar) }} > Make a bar graph
+            </Button>
 
-  const dataArray = []
-  const updatedDataArray = []
+            <Typography ml={6.5} variant="h4" component="h2">
+                {graphTitle}
+            </Typography>
 
- 
+            {[ChartTypes.Line, ChartTypes.Unknown].indexOf(displayedChartType) != -1 && (
+                <LineChart
+                    width={1200}
+                    height={400}
+                    data={moreData}
+                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis type="number" domain={[0, dataMax => (dataMax * 1.5)]} />
+                    <Tooltip />
+                    <Line
+                        type='monotone'
+                        strokeWidth={2}
+                        dataKey={'Latency'}
+                        stroke='#8884d8'
+                        fill='#8884d8'
+                    />
+                    <Line
+                        type='monotone'
+                        strokeWidth={2}
+                        dataKey={minLatency}
+                        stroke='#E1341E'
+                        fill='#8884d8'
+                    />
+                </LineChart>
 
-    const [minLatency, setMinLatency] = useState();
+            )}
 
-
-    // const header = "users";
-
-    const title = 'Users - Latency Information';
-
-    // const [metrics, setMetrics] = useState(dataArray);
-    const [graphTitle, setGraphTitle] = useState(title);
-  //set initial dataset values
-  
-
-
-// update the dataset values to use a different time frame
-slidersfor (i = 4; i >= 4 && i < 10; i++ ) {
-    updatedDataArray[(i-4)] = {
-        name: timeStamps[i], Latency: backendData[i].avg_latency}
-    }  
-
-
-
-  const header = backendData[0].service_type.toUpperCase()
-
-  const title = header + ' - Latency Information';
-
-  const [metrics, setMetrics] = useState(dataArray);
-  const [graphTitle, setGraphTitle] = useState(title);
-
-  
- 
-
-
-return (
-
-    
-    <div>
-        <Button variant="contained"
-            onClick={() => {setMoreData(
-            otherData); 
-            {setGraphTitle(metricsData[100].service_type + " - Latency Information")}}} > Change Data
-        </Button>
-        
-        <Button variant="contained"
-            onClick={() => {setMinLatency(
-            "MinLatency"); }} > Show Min Latency
-        </Button>
-
-        <Button id = "bar" variant="contained"
-            onClick={() => {setDisplayedChartType(ChartTypes.Bar) }} > Make a bar graph
-        </Button>
-
-        <Typography ml={6.5} variant="h4" component="h2">
-            {graphTitle}
-        </Typography>
-
-
-
-        {[ChartTypes.Line, ChartTypes.Unknown].indexOf(displayedChartType) != -1  && (
-
-            <LineChart
-                width={1200}
-                height={400}
-                data={moreData}
-                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis type="number" domain={["dataMin - 10", "dataMax + 10"]}/>
-                <Tooltip />
-                <Line   
-                    type='monotone'
-                    strokeWidth={2}
-                    dataKey={'Latency'}
-                    stroke='#8884d8'
-                    fill='#8884d8'
-                />
-                <Line
-                    type='monotone'
-                    strokeWidth={2}
-                    dataKey={minLatency}
-                    stroke='#E1341E'
-                    fill='#8884d8' 
-                />
-            </LineChart>
-
-        )}
-        {displayedChartType === ChartTypes.Bar && (
-
-            <BarChart width={730} height={250} data={moreData}>
+            {displayedChartType === ChartTypes.Bar && (
+                <BarChart width={730} height={250} data={moreData}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="name" />
                     <YAxis />
                     <Tooltip />
                     <Legend />
                     <Bar dataKey="Latency" fill="#8884d8" />
-                </BarChart> 
-
-        )}
-
-        {/* {displayedChartType === ChartTypes.Unknown && (
-            <div>
-                <button onClick={() => setDisplayedChartType(ChartTypes.Bar)}>Bar</button>
-                <button onClick={() => setDisplayedChartType(ChartTypes.Line)}>Line</button>
-            </div>
-        )} */}
-
-
-
-        
-        <LineChart
-
-            width={1200}
-
-            height={400}
-            data={moreData}
-            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis type="number" domain={[100, 300]}/>
-            <Tooltip />
-            <Line
-                type='monotone'
-                dataKey={'Latency'}
-                stroke='#8884d8'
-                fill='#8884d8'
-            />
-            <Line
-                type='monotone'
-                dataKey={minLatency}
-                stroke='#E1341E'
-                fill='#8884d8' 
-            />
-
-
-
-            {/* <Line
-                type='monotone'
-                dataKey={'amt'}
-                stroke='#E1341E'
-                fill='#6764d8' 
-            /> */}
-        </LineChart>
-   </div>
-
-
-)
-
+                </BarChart>
+            )}
+        </div>
+    )
 }
 
-
-
 export default Graph
-
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,10 @@
 import { useState, useEffect } from "react"
 import axios from "axios";
 import { useAuth } from "../hooks/auth";
-
 import { SimpleSelect } from "../components/SimpleSelect";
 import { SelectServices } from "../components/SelectServices";
-
 import Graph from '../components/Graph';
-
-import { Checkbox } from "@mui/material";
-
 import { TimeSlider } from "../components/TimeSlider";
-
-
 import LockIcon from '@mui/icons-material/Lock';
 import PersonIcon from '@mui/icons-material/Person';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
@@ -40,29 +33,8 @@ export const serviceMenuItems = [
 
 export const defValue = menuItems[3].value
 
-export const menuItems = [{label:"10 Minutes", value:"tenMinutes"}, {label:"1 Hour", value:"oneHour"},
-{label:"6 Hours", value:"sixHours"}, {label:"12 Hours", value:"twelvehours"}, {label:"24 Hours", value:"oneDay"}]
-
-export const filterMenuItems = [
-   {primary: "Status", key: "status"},
-   {primary: "Average Latency", key: "avglat"},
-   {primary: "Maximum Latency", key: "maxlat"},
-   {primary: "Minimum Latency", key: "minlat"},
-]
-
-export const serviceMenuItems = [ 
-{primary: "Authorization", icon: <LockIcon/>, key: "auth"},
-{primary: "User", icon: <PersonIcon/>, key: "user"},
-{primary: "Carts", icon: <ShoppingCartIcon/>, key: "cart"},
-{primary: "Products", icon: <Inventory2Icon/>, key: "products"},
-{primary: "Suggestions", icon:<AssistantPhotoIcon/>, key: "suggestions"},
-{primary: "Billing", icon: <CreditCardIcon/>, key: "billing"},
-]
-
-
 export const Home = () => {
     const { authenticatedRequest } = useAuth();
-
     const [isLoading, setIsLoading] = useState(true);
     const [account, setAccount] = useState(null);
     const [newData, setNewData] = useState(null);
@@ -78,15 +50,6 @@ export const Home = () => {
                     'Authorization': `Bearer ${token}`
                 }
             });
-
-
-            await axios.get('/metrics/getdata').then(res => {
-
-            // await axios.get('/services').then(res => {
-            //     const newData = JSON.stringify(res.data);
-            //     setNewData(newData)
-            //     return newData;
-            // });
 
             await axios.get('/metrics/getdata').then(res => {
                 console.log(res)
@@ -119,12 +82,7 @@ export const Home = () => {
                     <br></br>
                     <SimpleSelect onValueChange={(value) => setSelectedTime(value)} menuItems={menuItems} title="Time"/>
                 <br></br>
-
                 <SelectServices onServicesChange={setSelectedCheckValue} serviceMenuItems={serviceMenuItems} filterMenuItems={filterMenuItems}/>
-                <br/><br/>
-                <Graph/>
-                <br></br>
-                <br></br>
-               
+                <br/><br/>             
     </>);
 }
